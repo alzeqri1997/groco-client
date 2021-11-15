60d65e712fbbb95c947e095d0deeeabc5389dcf2
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2021-11-15 16:57:45 +0300

    add shopping-cart component

---
+++ src/components/Shopping-cart/shopping-cart.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {
+  faTimes,
+} from '@fortawesome/free-solid-svg-icons';
+
+import Box from './Box';
+
+import cart1 from '../../images/cart-1.jpg';
+import cart2 from '../../images/cart-2.jpg';
+import cart3 from '../../images/cart-3.jpg';
+
+
+
+const ShoppingCart = ({ isCartOpen }) => {
+  return (
+    <div className={`shopping-cart ${isCartOpen ? 'active' : ''}`}>
+
+      {
+        [{ img: cart1 }, { img: cart2 }, { img: cart3 }].map((box, index) =>
+          <Box img={box.img} icon={faTimes} key={index} />
+        )
+      }
+
+
+      <h3 className="total">
+        total : <span>56.97</span>
+      </h3>
+      <a href="#" className="btn">
+        checkout cart
+      </a>
+    </div>
+  )
+}
+
+export default ShoppingCart;
