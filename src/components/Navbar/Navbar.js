fccbaa56eb7021c50872e73373787f7bffb5bdfe
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2021-11-15 16:59:05 +0300

    add Navbar component with style

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,76 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { ReactComponent as Logo } from '../../assets/logo.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faBars,
+  faSearch,
+  faShoppingCart,
+  faUser,
+  faTimes,
+} from '@fortawesome/free-solid-svg-icons';
+import SearchBar from '../SearchBar';
+import ShoppingCart from '../Shopping-cart/shopping-cart';
+import Login from '../Login/login';
+import './Navbar.scss';
 const Navbar = () => {
-  return <h1>you are in Navbar</h1>;
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  const [isLoginOpen, setIsLoginOpen] = useState(false);
+
+  const handleSearch = () => {
+    setIsSearchOpen((prev) => !prev);
+    setIsCartOpen(false);
+    setIsLoginOpen(false);
+  };
+
+  const handleCart = () => {
+    setIsCartOpen((prev) => !prev);
+    setIsSearchOpen(false);
+    setIsLoginOpen(false);
+  };
+
+  const handleLogin = () => {
+    setIsLoginOpen((prev) => !prev);
+    setIsCartOpen(false);
+    setIsSearchOpen(false);
+  };
+
+  return (
+    <header className="header">
+      <a href="home.html" className="logo">
+        <Logo className="logo__img" /> groco
+      </a>
+      <nav className="navbar">
+        <a href="home.html">home</a>
+        <a href="shop.html">shop</a>
+        <a href="about.html">about</a>
+        <a href="review.html">review</a>
+        <a href="blog.html">blog</a>
+        <a href="contact.html">contact</a>
+      </nav>
+
+      <div className="icons">
+        <FontAwesomeIcon icon={faBars} id="menu-btn" />
+        <FontAwesomeIcon
+          icon={faSearch}
+          id="search-btn"
+          onClick={handleSearch}
+        />
+        <FontAwesomeIcon
+          icon={faShoppingCart}
+          id="cart-btn"
+          onClick={handleCart}
+        />
+        <FontAwesomeIcon icon={faUser} id="login-btn" onClick={handleLogin} />
+      </div>
+
+      <SearchBar isSearchOpen={isSearchOpen} />
+
+      <ShoppingCart isCartOpen={isCartOpen} />
+
+      <Login isLoginOpen={isLoginOpen} />
+    </header>
+  );
 };
 
 export default Navbar;
