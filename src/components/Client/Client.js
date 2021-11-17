73c9681c6d5f1142c912ad5a4ba994848e057748
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2021-11-17 06:25:12 +0300

    add Client review component with sytle

---
+++ src/components/Client/Client.js
@@ -0,0 +1,66 @@
+import React from 'react';
+
+import ClientItem from './ClientItem';
+
+import pic1 from '../../images/pic-1.png';
+import pic2 from '../../images/pic-2.png';
+import pic3 from '../../images/pic-3.png';
+import pic4 from '../../images/pic-4.png';
+import pic5 from '../../images/pic-5.png';
+import pic6 from '../../images/pic-6.png';
+
+const Client = () => {
+  const clientData = [
+    {
+      img: pic1,
+      name: 'john deo',
+      title: 'happy client',
+      comment:
+        'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem molestiae expedita culpa officiis harum impedit vel! Odit saepe similique vero!',
+    },
+    {
+      img: pic2,
+      name: 'john deo',
+      title: 'happy client',
+      comment:
+        'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem molestiae expedita culpa officiis harum impedit vel! Odit saepe similique vero!',
+    },
+    {
+      img: pic3,
+      name: 'john deo',
+      title: 'happy client',
+      comment:
+        'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem molestiae expedita culpa officiis harum impedit vel! Odit saepe similique vero!',
+    },
+    {
+      img: pic4,
+      name: 'john deo',
+      title: 'happy client',
+      comment:
+        'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem molestiae expedita culpa officiis harum impedit vel! Odit saepe similique vero!',
+    },
+    {
+      img: pic5,
+      name: 'john deo',
+      title: 'happy client',
+      comment:
+        'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem molestiae expedita culpa officiis harum impedit vel! Odit saepe similique vero!',
+    },
+    {
+      img: pic6,
+      name: 'john deo',
+      title: 'happy client',
+      comment:
+        'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem molestiae expedita culpa officiis harum impedit vel! Odit saepe similique vero!',
+    },
+  ];
+  return (
+    <section class="review">
+      {clientData.map((client, index) => (
+        <ClientItem {...client} key={index} />
+      ))}
+    </section>
+  );
+};
+
+export default Client;
