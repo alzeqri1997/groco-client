6c674ee29df120a35041a9c739ba9537347e7c4d
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2021-11-17 06:24:48 +0300

    add info component with style

---
+++ src/components/Info/Info.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import InfoItem from './InfoItem';
+
+import icon1 from '../../images/icon-1.png';
+import icon2 from '../../images/icon-2.png';
+import icon3 from '../../images/icon-3.png';
+
+const Info = () => {
+  const infoData = [
+    {
+      img: icon1,
+      title: 'fast delivery',
+      body: 'within 30 minutes',
+    },
+    {
+      img: icon2,
+      title: '24/7 available',
+      body: 'call us anytime',
+    },
+    {
+      img: icon3,
+      title: 'easy payment',
+      body: 'cash or credits',
+    },
+  ];
+  return (
+    <section class="info-container">
+      {infoData.map((info, index) => (
+        <InfoItem {...info} key={index} />
+      ))}
+    </section>
+  );
+};
+
+export default Info;
